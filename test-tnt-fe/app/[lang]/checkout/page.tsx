0abc531adfbb4f5d063e5000a2f320fa838fc391
Tirawat <98450270+Tirawat1@users.noreply.github.com>
2026-09-14 13:48:46 +0700

    feat: implement CheckoutView component for cart management and checkout process

--- test-tnt-fe/app/[lang]/checkout/page.tsx
+++ test-tnt-fe/app/[lang]/checkout/page.tsx
@@ -1,9 +1,14 @@
 import { getDictionary } from "@/lib/i18n/get-dictionary";
-import { CheckoutView } from "./CheckoutView";
+import { CheckoutView } from "../../../components/Checkout/CheckoutView";
 
 export default async function Page({ params }: PageProps<"/[lang]/checkout">) {
   const { lang } = await params;
   const dict = await getDictionary(lang);
 
-  return <CheckoutView cartTitle={dict.cart.cart} cartEmptyLabel={dict.cart.cart_empty} />;
+  return (
+    <CheckoutView
+      cartTitle={dict.cart.cart}
+      cartEmptyLabel={dict.cart.cart_empty}
+    />
+  );
 }
