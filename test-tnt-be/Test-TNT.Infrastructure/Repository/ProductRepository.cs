568478e16288e696fd79da7827537e9d5f2971b0
Tirawat <98450270+Tirawat1@users.noreply.github.com>
2026-09-14 13:13:31 +0700

    feat(product) : update order_by

--- test-tnt-be/Test-TNT.Infrastructure/Repository/ProductRepository.cs
+++ test-tnt-be/Test-TNT.Infrastructure/Repository/ProductRepository.cs
@@ -17,5 +17,5 @@ public class ProductRepository : IProductRepository
     public Task<List<Product>> GetAll() => _context.Products.ToListAsync();
 
     public Task<List<Product>> GetByIds(IReadOnlyCollection<int> ids) =>
-        _context.Products.Where(p => ids.Contains(p.Id)).ToListAsync();
+        _context.Products.Where(p => ids.Contains(p.Id)).OrderBy(p => p.Id).ToListAsync();
 }
